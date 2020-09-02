ec33806427c3271f3c993958c15cea5018362d6e
acerezci <alperencerezci@gmail.com>
2020-09-02 11:01:50 +0300

    initial commit

---
+++ src/Navigators/MainNavigator/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const MainNavigator = () => (
+  <View>
+    <Text>ALPEREN</Text>
+  </View>
+);
+
+export default MainNavigator;
